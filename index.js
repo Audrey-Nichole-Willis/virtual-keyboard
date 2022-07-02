20af9b50dca6aded5b5c87475135c7225e1f84d5
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-07-02 18:06:09 -0400

    set the initial values for the elements, the propeties, and the event handlers

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const Keyboard = {
-   
+   //the elements that we need to create are the main div, the keys container, and an array of keys.
     elements:{
-
+        main: null,
+        keysContainer: null,
+        keys: [],
     },
+    //the types of events that will be handled are input and close events.
     eventHandlers: {
-
+        oninput: null,
+        onclose: null
     },
+    //the properties of the keys will be:
     properties: {
-
+        values: "", 
+        capsLock: false
     },
     init() {
 
