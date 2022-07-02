3cfd44beed583f7116aa16d79524df574d25b9de
Audrey-Nichole-Willis <Audrey.Nichole.Willis@outlook.com>
2022-07-02 18:25:18 -0400

    wrote the init function in the keyboard object. it created and appended the elements as well as setting the the virtual keyboard as default for elements with a certain class

--- index.js
+++ index.js
@@ -16,7 +16,33 @@ const Keyboard = {
         capsLock: false
     },
     init() {
+        //create the div elements that will house the keys
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
 
+        //add classes to newly created elements
+        this.elements.main.classList.add("keyboard", "hidden-keyboard");
+        this.elements.keysContainer.classList.add("keyboard-keys");
+
+        //append the future rendered  keys to the key container
+        this.elements.keysContainer.appendChild(this.createBtnKeys());
+
+        //the keys are  all of the elements within the keysContainer with the class of keyboard-key
+        this.elements.keys = this.elements.keysContainer.querySelectorAll("keyboard-key");
+
+        //append elements  to the DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
+
+        //use the virtual keyboard on elements that have the class use-keyboard-input
+          document .querySelectorAll(".use-keyboard-input")
+            .forEach((element) => {
+              element.addEventListener("focus", () => {
+                this.open(element.value, (currentValue) => {
+                  element.value = currentValue;
+                });
+              });
+            });
     },
     createBtnKeys() {
 
